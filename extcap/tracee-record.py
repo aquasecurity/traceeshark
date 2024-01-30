68d3a0351ab403981367d43ef069ee79a03070e5
Ofek Shaked <ofek.shaked@aquasec.com>
2024-01-30 09:58:11 +0200

    Fixed some bugs, recording should now be stable

--- extcap/tracee-record.py
+++ extcap/tracee-record.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+from ctypes import cdll, byref, create_string_buffer
+import fcntl
 import os
 import select
 import signal
@@ -12,6 +14,13 @@ from threading import Thread
 
 DLT_USER0 = 147
 TRACEE_OUTPUT_PIPE = '/tmp/tracee_output.pipe'
+TRACEE_OUTPUT_PIPE_CAPACITY = 131072 # enough to hold the largest event encountered so far
+READER_COMM = 'tracee-record'
+TRACEE_LOGS_PATH = '/tmp/tracee_logs.log'
+
+DEFAULT_TRACEE_IMAGE = 'aquasec/tracee:latest'
+DEFAULT_DOCKER_OPTIONS = '--pid=host --cgroupns=host --privileged -v /etc/os-release:/etc/os-release-host:ro -v /var/run:/var/run:ro -v /sys/fs/cgroup:/sys/fs/cgroup -v /var/run/docker.sock:/var/run/docker.sock'
+DEFAULT_CONTAINER_NAME = 'tracee'
 
 container_id = None
 running = True
@@ -29,9 +38,10 @@ def show_interfaces():
 def show_config():
     args = []
 
-    args.append((0, '--image', 'Docker image', 'Tracee docker image to use', 'string', '{required=true}{default=aquasec/tracee:latest}{group=Container options}'))
-    args.append((1, '--name', 'Container name', 'Container name to use', 'string', '{default=tracee}{group=Container options}'))
-    args.append((2, '--docker-options', 'Docker options', 'Command line options for docker', 'string', '{default=--pid=host --cgroupns=host --privileged -v /etc/os-release:/etc/os-release-host:ro -v /var/run:/var/run:ro -v /sys/fs/cgroup:/sys/fs/cgroup -v /var/run/docker.sock:/var/run/docker.sock}{group=Container options}'))
+    args.append((0, '--image', 'Docker image', 'Tracee docker image to use', 'string', '{required=true}{default=%s}{group=Container options}' % DEFAULT_TRACEE_IMAGE))
+    args.append((1, '--name', 'Container name', 'Container name to use', 'string', '{default=%s}{group=Container options}' % DEFAULT_CONTAINER_NAME))
+    args.append((2, '--docker-options', 'Docker options', 'Command line options for docker', 'string', '{default=%s}{group=Container options}' % DEFAULT_DOCKER_OPTIONS))
+    args.append((4, '--tracee-options', 'Tracee options', 'Command line options for tracee', 'string', '{group=Tracee options}'))
 
     for arg in args:
         print("arg {number=%d}{call=%s}{display=%s}{tooltip=%s}{type=%s}%s" % arg)
@@ -69,6 +79,11 @@ def write_event(event, extcap_pipe):
     timestamp_secs = int(ts / 1000000000)
     timestamp_usecs = int((ts % 1000000000) / 1000)
 
+    # TODO: temporary workaround for tracee timestamp bug
+    if timestamp_secs < 0 or timestamp_secs > 2**32-1:
+        timestamp_secs = 0
+        timestamp_usecs = 0
+
     packet += struct.pack('<L', timestamp_secs) # timestamp seconds
     packet += struct.pack('<L', timestamp_usecs)  # timestamp microseconds
     packet += struct.pack('<L', caplen)  # length captured
@@ -79,11 +94,23 @@ def write_event(event, extcap_pipe):
     extcap_pipe.write(packet)
 
 
+def set_proc_name(newname: str):
+    libc = cdll.LoadLibrary('libc.so.6')
+    buff = create_string_buffer(len(newname)+1)
+    buff.value = newname.encode()
+    libc.prctl(15, byref(buff), 0, 0, 0)
+
+
 def read_output(logs_pipe, extcap_pipe):
     global running
 
+    # change our process name so we can exclude it (otherwise it may flood capture with pipe read activity)
+    # TODO: using a PID is more robust, but currently there is no way to filter by PID in namespace (required when running on WSL)
+    set_proc_name(READER_COMM)
+
     # open tracee logs pipe and extcap pipe
     logs_pipe_f = os.open(logs_pipe, os.O_RDONLY, os.O_NONBLOCK)
+    fcntl.fcntl(logs_pipe_f, fcntl.F_SETPIPE_SZ, TRACEE_OUTPUT_PIPE_CAPACITY)
     extcap_pipe_f = open(extcap_pipe, 'wb')
 
     # write fake PCAP header
@@ -96,8 +123,12 @@ def read_output(logs_pipe, extcap_pipe):
 
         # there is data available to read
         if rlist:
-            event = os.read(logs_pipe_f, 65536) # sensible max event size
-            write_event(event, extcap_pipe_f)
+            data = os.read(logs_pipe_f, TRACEE_OUTPUT_PIPE_CAPACITY)
+
+            # split read data into individual events (TODO: this method might hurt perf, might want to search for newlines while reading)
+            for event in data.split(b'\n'):
+                if len(event) > 0:
+                    write_event(event, extcap_pipe_f)
         else:
             continue
     
@@ -118,8 +149,8 @@ def stop_capture(signum, frame):
     proc = subp.Popen(['/bin/sh', '-c', command], stdout=subp.PIPE, stderr=subp.PIPE)
     _, err = proc.communicate()
     if proc.returncode != 0:
-        sys.stderr.write(f'docker wait returned with error code {proc.returncode}, stderr dump:\n{err}')
-        sys.exit(0)
+        sys.stderr.write(f'docker kill returned with error code {proc.returncode}, stderr dump:\n{err}')
+        sys.exit(1)
 
 
 def tracee_capture(args: argparse.Namespace):
@@ -141,12 +172,17 @@ def tracee_capture(args: argparse.Namespace):
     reader_th = Thread(target=read_output, args=(TRACEE_OUTPUT_PIPE, args.fifo), daemon=True)
     reader_th.start()
 
-    command = 'docker run -d --rm'
+    command = 'docker run -d'
 
-    if len(args.name) >= 1:
+    if len(args.name) > 0:
         command += f' --name {args.name}'
     
-    command += f' {args.docker_options} -v {TRACEE_OUTPUT_PIPE}:/output.pipe:rw {args.image} -o json:/output.pipe'
+    command += f' {args.docker_options} -v {TRACEE_OUTPUT_PIPE}:/output.pipe:rw -v {TRACEE_LOGS_PATH}:/logs.log:rw {args.image}'
+
+    if args.tracee_options and len(args.tracee_options) > 0:
+        command += f' {args.tracee_options}'
+    
+    command += f" --scope comm!='{READER_COMM}' -o json:/output.pipe --log file:/logs.log"
 
     signal.signal(signal.SIGINT, stop_capture)
     signal.signal(signal.SIGTERM, stop_capture)
@@ -167,7 +203,28 @@ def tracee_capture(args: argparse.Namespace):
     if proc.returncode != 0:
         sys.stderr.write(f'docker wait returned with error code {proc.returncode}, stderr dump:\n{err}')
         sys.exit(1)
+    
+    # check tracee logs for errors
+    command = f'docker logs {container_id}'
 
+    proc = subp.Popen(['/bin/sh', '-c', command], stdout=subp.PIPE, stderr=subp.PIPE)
+    _, logs_err = proc.communicate()
+    if proc.returncode != 0:
+        sys.stderr.write(f'docker logs returned with error code {proc.returncode}, stderr dump:\n{err}')
+        sys.exit(1)
+    
+    command = f'docker rm {container_id}'
+
+    proc = subp.Popen(['/bin/sh', '-c', command], stdout=subp.PIPE, stderr=subp.PIPE)
+    _, err = proc.communicate()
+    if proc.returncode != 0:
+        sys.stderr.write(f'docker rm returned with error code {proc.returncode}, stderr dump:\n{err}')
+        sys.exit(1)
+    
+    if len(logs_err) > 0:
+        sys.stderr.write(f'Tracee exited with error message:\n{logs_err.decode()}')
+        sys.exit(1)
+        
 
 def main():
     parser = argparse.ArgumentParser(prog=os.path.basename(__file__), description='Record events and packets using Tracee')
@@ -182,9 +239,10 @@ def main():
     parser.add_argument("--fifo", help="Use together with capture to provide the fifo to dump data to")
 
     # custom arguments
-    parser.add_argument('--image', type=str)
-    parser.add_argument('--name', type=str)
-    parser.add_argument('--docker-options', type=str)
+    parser.add_argument('--image', type=str, default=DEFAULT_TRACEE_IMAGE)
+    parser.add_argument('--name', type=str, default=DEFAULT_CONTAINER_NAME)
+    parser.add_argument('--docker-options', type=str, default=DEFAULT_DOCKER_OPTIONS)
+    parser.add_argument('--tracee-options', type=str)
 
     args = parser.parse_args()
 
