6b308f48faea7cf0dab1f1a00610ed24d9b5bdea
Ofek Shaked <32914127+oshaked1@users.noreply.github.com>
2024-06-02 18:16:15 +0300

    Added workflow for creating a release build (#1)

--- scripts/make_cmake.bat
+++ scripts/make_cmake.bat
@@ -5,17 +5,14 @@ FOR /F "tokens=*" %%i in ('type .env') do SET %%i
 rmdir /S /Q wireshark\plugins\epan\tracee-event
 rmdir /S /Q wireshark\plugins\epan\tracee-network-capture
 rmdir /S /Q wireshark\plugins\wiretap\tracee-json
-mkdir wireshark\plugins\epan\tracee-event
-mkdir wireshark\plugins\epan\tracee-network-capture
-mkdir wireshark\plugins\wiretap\tracee-json
-copy /Y plugins\epan\tracee-event wireshark\plugins\epan\tracee-event
-copy /Y plugins\epan\tracee-network-capture wireshark\plugins\epan\tracee-network-capture
-copy /Y plugins\wiretap\tracee-json wireshark\plugins\wiretap\tracee-json
+del /Q wireshark\plugins\epan\common.h
+del /Q wireshark\plugins\epan\wsjson_extensions.c
+xcopy /Y /E plugins wireshark\plugins\
 copy /Y CMakeListsCustom.txt wireshark
 rmdir /S /Q build
 mkdir build
 pushd build
-cmake -G "Visual Studio 17 2022" -A x64 -DTRACEESHARK_VERSION=%TRACEESHARK_VERSION% -DENABLE_CCACHE=Yes ..\wireshark
+cmake -G "Visual Studio 17 2022" -A x64 -DTRACEESHARK_VERSION=%TRACEESHARK_VERSION% ..\wireshark
 popd
 
 endlocal
