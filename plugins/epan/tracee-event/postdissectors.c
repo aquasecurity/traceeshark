edd3eed273e99ca244bbf281d267bcfcdfe8c7d2
Ofek Shaked <ofek.shaked@aquasec.com>
2024-06-23 14:04:35 +0300

    Added postdissector for security_socket_bind and security_socket_connect

--- plugins/epan/tracee-event/postdissectors.c
+++ plugins/epan/tracee-event/postdissectors.c
@@ -5,16 +5,6 @@ static dissector_handle_t event_postdissector;
 
 wmem_map_t *event_postdissectors;
 
-static int dissect_sig_machine_fingerprint(tvbuff_t *tvb _U_, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
-{
-    const gchar *file_path;
-
-    if ((file_path = wanted_field_get_str("tracee.args.File_path")) != NULL)
-        col_add_fstr(pinfo->cinfo, COL_INFO, "%s was opened", file_path);
-    
-    return 0;
-}
-
 static int dissect_sched_process_exec(tvbuff_t *tvb _U_, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
 {
     const gchar *pathname, *cmdline;
@@ -106,6 +96,33 @@ static int dissect_net_packet_http(tvbuff_t *tvb _U_, packet_info *pinfo, proto_
     return 0;
 }
 
+static int dissect_security_socket_bind_connect(tvbuff_t *tvb _U_, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
+{
+    const gchar *family, *addr, *port;
+
+    family = wanted_field_get_str("tracee.sockaddr.sa_family");
+
+    if (family == NULL)
+        return 0;
+    
+    if (strcmp(family, "AF_INET") == 0) {
+        addr = wanted_field_get_str("tracee.sockaddr.sin_addr");
+        port = wanted_field_get_str("tracee.sockaddr.sin_port");
+    }
+    else if (strcmp(family, "AF_INET6") == 0) {
+        addr = wanted_field_get_str("tracee.sockaddr.sin6_addr");
+        port = wanted_field_get_str("tracee.sockaddr.sin6_port");
+    }
+    else
+        return 0;
+    
+    if (addr && port) {
+        col_add_fstr(pinfo->cinfo, COL_INFO, "Address: %s, Port: %s", addr, port);
+    }
+
+    return 0;
+}
+
 static int postdissect_event(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 {
     const char *event_name;
@@ -131,9 +148,6 @@ static void register_wanted_fields(void)
     // needed for general postdissector
     register_wanted_field("tracee.eventName");
 
-    // needed for dissect_sig_machine_fingerprint
-    register_wanted_field("tracee.args.File_path");
-
     // needed for dissect_sched_process_exec
     register_wanted_field("tracee.args.pathname");
     register_wanted_field("tracee.args.command_line");
@@ -150,6 +164,13 @@ static void register_wanted_fields(void)
     register_wanted_field("tracee.proto_http.protocol");
     register_wanted_field("tracee.proto_http.uri_path");
     register_wanted_field("tracee.proto_http.status");
+
+    // needed for dissect_security_socket_bind_connect
+    register_wanted_field("tracee.sockaddr.sa_family");
+    register_wanted_field("tracee.sockaddr.sin_addr");
+    register_wanted_field("tracee.sockaddr.sin_port");
+    register_wanted_field("tracee.sockaddr.sin6_addr");
+    register_wanted_field("tracee.sockaddr.sin6_port");
 }
 
 void register_tracee_postdissectors(int proto)
@@ -159,10 +180,11 @@ void register_tracee_postdissectors(int proto)
 
     event_postdissectors = wmem_map_new(wmem_epan_scope(), g_str_hash, g_str_equal);
 
-    register_tracee_event_postdissector("sig_machine_fingerprint", dissect_sig_machine_fingerprint);
     register_tracee_event_postdissector("sched_process_exec", dissect_sched_process_exec);
     register_tracee_event_postdissector("net_packet_http_request", dissect_net_packet_http_request);
     register_tracee_event_postdissector("net_packet_http", dissect_net_packet_http);
+    register_tracee_event_postdissector("security_socket_bind", dissect_security_socket_bind_connect);
+    register_tracee_event_postdissector("security_socket_connect", dissect_security_socket_bind_connect);
 
     register_wanted_fields();
 }
