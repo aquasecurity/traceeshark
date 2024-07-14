a3908198b044bffe0d794b3d2f17370da55b681f
Ofek Shaked <ofek.shaked@aquasec.com>
2024-07-14 12:30:14 +0300

    Windows build: simplify file copying Use robocopy for syncing directories instead of manually copying files. Also removed the "make" prefix from the names of the scripts

---
+++ scripts/build.bat
@@ -0,0 +1,17 @@
+@echo off
+
+if not exist "build" (
+    echo Build directory doesn't exist, run "scripts\make_cmake.bat" first
+    exit /b 1
+)
+
+robocopy .\ wireshark CMakeListsCustom.txt /COPY:DAT
+robocopy plugins\epan wireshark\plugins\epan common.h /COPY:DAT
+robocopy plugins\epan wireshark\plugins\epan wsjson_extensions.c /COPY:DAT
+robocopy "plugins\epan\tracee-event" "wireshark\plugins\epan\tracee-event" /MIR /COPY:DAT
+robocopy "plugins\epan\tracee-network-capture" "wireshark\plugins\epan\tracee-network-capture" /MIR /COPY:DAT
+robocopy "plugins\wiretap\tracee-json" "wireshark\plugins\wiretap\tracee-json" /MIR /COPY:DAT
+
+pushd build
+msbuild /m /p:Configuration=RelWithDebInfo Wireshark.sln
+popd
